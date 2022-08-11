c32446b1d2a70f6284ec67713bc0759851db4e87
geekymv <ym2011678@foxmail.com>
2022-08-11 18:45:15 +0800

    添加注释

--- apps/nsqd/main.go
+++ apps/nsqd/main.go
@@ -32,17 +32,20 @@ func main() {
 
 func (p *program) Init(env svc.Environment) error {
 	opts := nsqd.NewOptions()
-
+	// 解析命令行参数
 	flagSet := nsqdFlagSet(opts)
 	flagSet.Parse(os.Args[1:])
 
+	// 使用时间作为随机种子值
 	rand.Seed(time.Now().UTC().UnixNano())
 
+	// 启动命令 `nsqd -version` 用于打印版本号，并退出
 	if flagSet.Lookup("version").Value.(flag.Getter).Get().(bool) {
 		fmt.Println(version.String("nsqd"))
 		os.Exit(0)
 	}
 
+	// 读取配置文件
 	var cfg config
 	configFile := flagSet.Lookup("config").Value.String()
 	if configFile != "" {
@@ -52,8 +55,9 @@ func (p *program) Init(env svc.Environment) error {
 		}
 	}
 	cfg.Validate()
-	// 将 flagSet 和 cfg 的配置信息合并到 opts
+	// 将 flagSet 和 config 的配置信息合并到 opts
 	options.Resolve(opts, flagSet, cfg)
+
 	// 创建 nsqd
 	nsqd, err := nsqd.New(opts)
 	if err != nil {
@@ -65,10 +69,12 @@ func (p *program) Init(env svc.Environment) error {
 }
 
 func (p *program) Start() error {
+	// 加载元数据
 	err := p.nsqd.LoadMetadata()
 	if err != nil {
 		logFatal("failed to load metadata - %s", err)
 	}
+	// 持久化元数据
 	err = p.nsqd.PersistMetadata()
 	if err != nil {
 		logFatal("failed to persist metadata - %s", err)
