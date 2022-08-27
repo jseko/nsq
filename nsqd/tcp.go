bd2daebb767c6b89e2a20a0cc544a22885a6de96
geekymv <ym2011678@foxmail.com>
2022-08-27 10:36:24 +0800

    读取4字节的版本号

--- nsqd/tcp.go
+++ nsqd/tcp.go
@@ -31,6 +31,7 @@ func (p *tcpServer) Handle(conn net.Conn) {
 	// the version of the protocol that it intends to communicate, this will allow us
 	// to gracefully upgrade the protocol away from text/line oriented to whatever...
 	buf := make([]byte, 4)
+	// 会阻塞，直到读取够4字节（  V2）
 	_, err := io.ReadFull(conn, buf)
 	if err != nil {
 		p.nsqd.logf(LOG_ERROR, "failed to read protocol version - %s", err)
